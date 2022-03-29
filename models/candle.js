2db18a8eb96860922a499ddc780e145b5aa76075
Aqsa Saleheene <angieclrk21@gmail.com>
2022-03-29 16:22:27 -0400

    added half of herko

--- models/candle.js
+++ models/candle.js
@@ -3,9 +3,7 @@ const { Schema } = require('mongoose')
 const Candle = new Schema(
   {
     name: { type: String, required: true },
-    location: { type: String, required: true },
-    url: { type: String, required: true },
-    body: { type: String, required: true },
+    description: { type: String, required: true },
     candle_id: { type: Schema.Types.ObjectId, ref: 'candle_id' }
   },
   { timestamps: true }
