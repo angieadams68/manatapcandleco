f146b347a34b7e1e20dfbb5a0929564c20ad8ade
Aqsa Saleheene <angieclrk21@gmail.com>
2022-03-30 11:32:19 -0400

    server stufff

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { Candle, Review } = require('./models')
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
@@ -6,17 +7,46 @@ const db = require('./db')
 
 const app = express()
 
+
+/////////////// MIDDLEWARE ////////////////
+
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'))
+
+
+//////// ROUTES ////////
 
 app.get('/', (req, res) => {
   res.send('This is root!')
 })
 
+
+app.get("/candles", async (req, res) => {
+    const candles = await Candle.find({});
+    res.json(candles);
+  });
+
+
+
+
+  app.get("/reviews", async (req, res) => {
+    const reviews = await Review.find({});
+    res.json(reviews);
+  });
+
+
+
+
+
+/////////////// EXPRESS SERVER LISTEN TO PORT //////////////////
+
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
 })
+
+
 
 
 
